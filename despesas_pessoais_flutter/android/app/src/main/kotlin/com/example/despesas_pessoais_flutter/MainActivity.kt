6f76a3482bba57ad9a35706c6ca141846e13caa3
brilhante <brilhante@pop-os.localdomain>
2021-09-18 14:59:23 -0300

    PokeDex flutter

--- despesas_pessoais_flutter/android/app/src/main/kotlin/com/example/despesas_pessoais_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.despesas_pessoais_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- navegacao_em_multiplas_telas_flutter/android/app/src/main/kotlin/com/example/navegacao_em_multiplas_telas_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.navegacao_em_multiplas_telas_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
